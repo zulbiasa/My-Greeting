b568001a3648834489cf19f7e9c1ae8f1d3fe563
Administrator <Administrator@PSPKM2826-2254>
2022-03-28 11:36:03 +0800

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Dim message As String
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Now.DayOfWeek = System.DayOfWeek.Monday Then
+            message = "Have a nice week"
+        Else
+            If Now.DayOfWeek >= System.DayOfWeek.Tuesday And
+            Now.DayOfWeek <= System.DayOfWeek.Friday Then
+                message = "Welcome back!"
+            Else
+                message = "Have a nice weekend!"
+            End If
+        End If
+        MessageBox.Show(message, "If Statement", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Select Case Now.DayOfWeek
+            Case System.DayOfWeek.Monday
+                message = "Have a nice week"
+            Case System.DayOfWeek.Tuesday, System.DayOfWeek.Wednesday,
+            System.DayOfWeek.Thursday, System.DayOfWeek.Friday
+                message = "Welcome back!"
+            Case System.DayOfWeek.Saturday, System.DayOfWeek.Sunday
+                message = "Have a nice weekend!"
+        End Select
+        MessageBox.Show(message, "Case Statement", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+    End Sub
+End Class
